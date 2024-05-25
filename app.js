59825739b60b6e096d7d214c31eeb18245df7922
geunseonkim <guensoen@gmail.com>
2024-05-25 14:02:45 +1200

    release todo-bd

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const port = 4000;
 const indexRouter = require('./routes/index')
+require('dotenv').config()
 
 const app = express()
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+// console.log("mongoURI", MONGODB_URI_PROD)
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/api', indexRouter)
 
-const mongoURI = `mongodb://localhost:27017/todoList`
+// const mongoURI = `mongodb://localhost:27017/todoList`
+const mongoURI = MONGODB_URI_PROD
 mongoose.connect(mongoURI).then(() => {
     console.log("mongoose connected")
 }).catch((err) => {
