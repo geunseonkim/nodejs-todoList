e01690445ea28803f71892d6f15226801aa80107
geunseonkim <guensoen@gmail.com>
2024-05-27 20:42:09 +1200

    delete console

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -9,21 +9,21 @@ userController.createUser = async(req, res) => {
     try{
         const {email, name, password} = req.body
         const user = await User.findOne({email})
-        console.log("user", user) // user DB or null;
         if (user) {
             throw new Error ("you're already a registered user")
         }
         const salt = bcrypt.genSaltSync(saltRounds)
         const hash = bcrypt.hashSync(password, salt)
-        console.log("hash", hash)
         const newUser = User({email, name, password: hash})
         await newUser.save()
         res.status(200).json({status: "success"})
     } catch (err) {
-        res.status(400).json({status: "fail", error: err.message}) // error: err를 해주면 "error": {} 이렇게만 뜨는데, Error 객체를 생성할 때 문자열로만 에러 메시지를 지정해주었기 때문임
+        res.status(400).json({status: "fail", error: err.message})
+        // error: err라고 해주면 "error": {} 이렇게만 뜨는데, Error 객체를 생성할 때 문자열로만 에러 메시지를 지정해주었기 때문임
     }
 }
 
+// 로그인
 userController.loginWithEmail = async(req, res) => {
     try{
         const {email, password} = req.body
